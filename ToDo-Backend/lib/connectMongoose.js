ea94c607deb089b24f4ca1123269b4f9bd872ea9
Fran Mackinlay <franmackinlay@gmail.com>
2020-05-07 15:51:49 -0300

    mongoose client created

---
+++ ToDo-Backend/lib/connectMongoose.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const conn = mongoose.connection;
+
+conn.on('open', () => {
+  console.log('Conectado a MongoDB en', conn.name);
+});
+
+conn.on('error', (err) => {
+  console.error('Error de conexión: ', err);
+  process.exit(1);
+});
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+
+module.exports = conn;
